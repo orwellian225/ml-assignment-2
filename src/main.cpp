c98af344da8b1f785a881ccf81c4581cefee972d
Orwellian <orwellian225@gmail.com>
2023-05-18 21:58:36 +0200

    add: misc setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+
 #include <fmt/core.h>
+
+/* NOTE TO SELF ON CMAKE:
+ *  Add new .cpp files to the cmake file and then rebuild the make file
+*/
 
 int main() {
     fmt::print("Hello, World!");
