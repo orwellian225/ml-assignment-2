04e77ec16e60f33975252e0375a2e381fdee94ff
Orwellian <orwellian225@gmail.com>
2023-05-20 17:51:06 +0200

    add: Broken Forward Prop

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,39 @@
 int main() {
 
     nnlayer_t test_layer = init_layer(5, 2);
-    test_layer.print();
+    // test_layer.print();
 
-    auto activation_LU = [](double x) { return x; };
-    auto activation_S = [](double x) { return 1.0 / (1.0 + std::exp(-1.0 * x)); };
-    auto activation_ReLU = [](double x) { return x > 0 ? x : 0; };
+    std::vector<size_t> test_structure{3, 2, 1};
+    nn_t test_network = init_network(test_structure);
+    test_network.print();
 
-    Eigen::VectorXd input {{2.0, 3.0, 4.0, 5.0, 6.0}};
-    Eigen::VectorXd result_lu = test_layer.fprop_layer(input, activation_LU);
-    Eigen::VectorXd result_s = test_layer.fprop_layer(input, activation_S);
-    Eigen::VectorXd result_relu = test_layer.fprop_layer(input, activation_ReLU);
+    // auto activation_lu = [](double x) { return x; };
+    // auto activation_s = [](double x) { return 1.0 / (1.0 + std::exp(-1.0 * x)); };
+    // auto activation_relu = [](double x) { return x > 0 ? x : 0; };
 
-    fmt::print("Input Vector:\n{}\n", input);
-    fmt::print("Linear Unit Result Vector:\n{}\n", result_lu);
-    fmt::print("Sigmoid Unit Result Vector:\n{}\n", result_s);
-    fmt::print("ReLU Result Vector:\n{}\n", result_relu);
+    auto activation_lu = [](const Eigen::VectorXd& values) { return values.unaryExpr([](double x) { return x; }); };
+    auto activation_sig = [](const Eigen::VectorXd& values) { return values.unaryExpr([](double x) { return 1.0 / (1.0 + std::exp(-x)); }); };
+    auto activation_relu = [](const Eigen::VectorXd& values) { return values.unaryExpr([](double x) { return x > 0 ? x : 0; }); };
+    auto activation_softmax = [](const Eigen::VectorXd& values) { 
+        Eigen::VectorXd exp_vals = values.unaryExpr([](double x) { return std::exp(x); });
+        const double exp_sum = exp_vals.sum();
+        return exp_vals.unaryExpr([exp_sum](double x) { return x / exp_sum; });
+    };
+
+    Eigen::VectorXd input {{2.0, 3.0, 4.0 }};
+    // Eigen::VectorXd result_lu = test_layer.fprop_layer(input, activation_lu);
+    // Eigen::VectorXd result_sig = test_layer.fprop_layer(input, activation_sig);
+    // Eigen::VectorXd result_relu = test_layer.fprop_layer(input, activation_relu);
+    // Eigen::VectorXd result_softmax = test_layer.fprop_layer(input, activation_softmax);
+
+    // fmt::print("Input Vector:\n{}\n", input);
+    // fmt::print("Linear Unit Result Vector:\n{}\n", result_lu);
+    // fmt::print("Sigmoid Unit Result Vector:\n{}\n", result_sig);
+    // fmt::print("ReLU Result Vector:\n{}\n", result_relu);
+    // fmt::print("Softmax Result Vector:\n{}\n", result_softmax);
+
+    Eigen::VectorXd fprop_result = test_network.eval_network(input, activation_relu);
+    fmt::print("Test fprop result\n{}\n", fprop_result);
 
     return 0;
 }
--- src/neuralnet.cpp
+++ src/neuralnet.cpp
@@ -25,13 +25,13 @@ nnlayer_t init_layer(size_t in_count, size_t out_count) {
     };
 }
 
-Eigen::VectorXd nnlayer_t::fprop_layer(Eigen::VectorXd input, std::function<double(double)> activate_f) {
+Eigen::VectorXd nnlayer_t::fprop_layer(const Eigen::VectorXd& input, std::function<Eigen::VectorXd(Eigen::VectorXd)> activate_f) {
     Eigen::VectorXd input_with_bias(input.size() + 1);
     Eigen::Vector<double, 1> bias(1.0);
     input_with_bias << bias, input; 
 
     Eigen::VectorXd result = this->weights * input_with_bias;
-    result = result.unaryExpr(activate_f);
+    result = activate_f(result);
 
     return result;
 }
@@ -41,4 +41,54 @@ void nnlayer_t::print() {
         "Network Layer:\nInput neuron count {}\nOutput neuron count {}\nWeights\n{}\n",
         this->input_count, this->output_count, this->weights
     );
+}
+
+nn_t init_network(std::vector<size_t> network_structure) {
+    std::vector<nnlayer_t> layers;
+
+    for (size_t l = 1; l < network_structure.size(); ++l) {
+        nnlayer_t new_layer = init_layer(network_structure[l-1], network_structure[l]);
+        layers.push_back(new_layer);
+    }
+
+    size_t max_layer_node = *std::max_element(network_structure.begin(), network_structure.end());
+    size_t layer_count = network_structure.size();
+
+    return nn_t {
+        network_structure,
+        layers,
+        max_layer_node,
+        layer_count
+    };
+}
+
+Eigen::MatrixXd nn_t::fprop_network(const Eigen::VectorXd& input, std::function<Eigen::VectorXd(Eigen::VectorXd)> activate_f) {
+    Eigen::MatrixXd activation_matrix(layer_count, max_layer_nodes);
+
+    activation_matrix.row(0) = input;
+    for (size_t l = 1; l < layer_count; ++l) {
+        activation_matrix.row(l) = layers[l].fprop_layer(activation_matrix.row(l - 1), activate_f);
+    }
+
+    return activation_matrix;
+}
+
+Eigen::VectorXd nn_t::eval_network(const Eigen::VectorXd& input, std::function<Eigen::VectorXd(Eigen::VectorXd)> activate_f) {
+    Eigen::MatrixXd activation_matrix = fprop_network(input, activate_f);
+    return activation_matrix.row(layer_count - 1);
+}
+
+Eigen::VectorXd nn_t::eval_network(const Eigen::VectorXd& input, std::function<Eigen::VectorXd(Eigen::VectorXd)> activate_f, std::function<Eigen::VectorXd(Eigen::VectorXd)> classify_f) {
+    Eigen::VectorXd activation_result = eval_network(input, activate_f);
+    return classify_f(activation_result);
+}
+
+void nn_t::print() {
+    fmt::print("Neural Network:\n");
+    fmt::print("Network Structure: ({})\n", fmt::join(structure, " "));
+    fmt::print("Network Layers:\n");
+
+    for (auto layer: layers) {
+        layer.print();
+    }
 }
