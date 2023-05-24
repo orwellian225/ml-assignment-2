d305c839dc18ed1f434b5677b69ab64114608ddd
Ashlea <smithashlea@gmail.com>
2023-05-24 16:40:55 +0200

    PCA
    add: custom PCA

--- scripts/PCA.py
+++ scripts/PCA.py
@@ -17,7 +17,7 @@ def using_PCA_library(features):
     print(features_scaled.shape)
     #apply PCA
 
-    pca = PCA()
+    pca = PCA(n_components=53)
     features_pca = pd.DataFrame(pca.fit_transform(features_scaled))
     print("Features PCA")
     print(features_pca)
@@ -27,9 +27,9 @@ def using_PCA_library(features):
     exp_var_pca = pca.explained_variance_ratio_
     print("Explained variance")
     print(exp_var_pca)
+    
     variance =0
     count=0
-    
     for i in exp_var_pca:
         variance +=i
         count+=1
@@ -75,34 +75,155 @@ def PCA_custom(features):
     print(cov_matrix.shape)
     #
     # Determine eigenvalues and eigenvectors
+    # eigenvectors represent the principle components that contain most of the information (variance) represented using features
     #
-    egnvalues, egnvectors = eigh(cov_matrix)
+    eigenDecom = eigh(cov_matrix)
+    egnvalues = eigenDecom[0]
+    egnvectors = eigenDecom[1]
+    
+    idx = np.argsort(egnvalues)[::-1]
+    egnvalues = egnvalues[idx]
+    egnvectors = egnvectors[:,idx]
+    
     #
     # Determine explained variance
     #
     total_egnvalues = sum(egnvalues)
-    var_exp = [(i/total_egnvalues) for i in sorted(egnvalues, reverse=True)]
+    #var_exp = [(i/total_egnvalues) for i in sorted(egnvalues, reverse=True)]
+    var_exp = [(i/total_egnvalues) for i in egnvalues]
     var_exp_display = pd.DataFrame(var_exp)
     print("Variance Explained: ")
     print(var_exp_display)
     
-    #
+    variance =0
+    count=0
+    for i in var_exp:
+        variance +=i
+        count+=1
+        if (variance >=0.8):
+            break
+    
+    print(variance) 
+    print("Amount of features that make up 0.8 of variance: ", count)
+    
+    #Forming feature matrix
+    
+    dimensions = count+1
+    adjustedData = X_train_std
+    featureMatrix = egnvectors[:,1:dimensions]
+    print(featureMatrix)
+    #deriving new dataSet
+    finalData = np.matmul(np.transpose(featureMatrix),np.transpose(adjustedData))
+    finalData = np.transpose(finalData)
+    print(pd.DataFrame(finalData))
+    
     # Plot the explained variance against cumulative explained variance
     #
     import matplotlib.pyplot as plt
     cum_sum_exp = np.cumsum(var_exp)
     plt.bar(range(0,len(var_exp)), var_exp, alpha=0.5, align='center', label='Individual explained variance')
-    plt.step(range(0,len(cum_sum_exp)), cum_sum_exp, where='mid',label='Cumulative explained variance')
+    #plt.step(range(0,len(cum_sum_exp)), cum_sum_exp, where='mid',label='Cumulative explained variance')
     plt.ylabel('Explained variance ratio')
     plt.xlabel('Principal component index')
     plt.legend(loc='best')
     #plt.tight_layout()
     plt.show()
+    
+   
 
-#PCA_custom(features)
-using_PCA_library(features)
+PCA_custom(features)
 
 
 
+
+def myplot(score, coeff, labels=None):
+        xs = score[:,0]
+        ys = score[:,1]
+        n = coeff.shape[0]
+        scalex = 1.0/(xs.max() - xs.min())
+        scaley = 1.0/(ys.max() - ys.min())
+        plt.scatter(xs * scalex,ys * scaley, c = labels)
+        for i in range(n):
+            plt.arrow(0, 0, coeff[i,0], coeff[i,1],color = 'r',alpha = 0.5)
+            if labels is None:
+                plt.text(coeff[i,0]* 1.15, coeff[i,1] * 1.15, "Var"+str(i+1), color = 'g', ha = 'center', va = 'center')
+            else:
+                plt.text(coeff[i,0]* 1.15, coeff[i,1] * 1.15, labels[i], color = 'b', ha = 'center', va = 'center')
+        plt.xlim(-1,1)
+        plt.ylim(-1,1)
+        plt.xlabel("PC{}".format(1))
+        plt.ylabel("PC{}".format(2))
+        plt.grid()
+        
+def PCA_analysis(features,labels):
+    y = labels
+    scalar = StandardScaler()
+    features_scaled = scalar.fit_transform(features)
+    
+    pca = PCA()
+    features_new = pca.fit_transform(features_scaled)
+    
+    myplot(features_new[:,0:2], np.transpose(pca.components_[0:2, :]))
+    plt.show()
+    
+    print("Explained Variance")
+    print(pca.explained_variance_ratio_)
+    
+    print()
+    print("Components")
+    print(abs(pca.components_))
+    
+
+def important_features(features):
+    
+    #z-score the features
+    scalar = StandardScaler()
+    scalar.fit(features)
+    features = scalar.transform(features)
+    
+    #PCA model
+    pca = PCA()
+    feature_new = pca.fit_transform(features)
+    
+    #plotting data before and after PCA transform
+    fig, axes = plt.subplots(1,2)
+    axes[0].scatter(features[:,0], features[:,1], c=labels)
+    axes[0].set_xlabel('x1')
+    axes[0].set_ylabel('x2')
+    axes[0].set_title('Before PCA')
+    axes[1].scatter(feature_new[:,0], feature_new[:,1], c=labels)
+    axes[1].set_xlabel('PC1')
+    axes[1].set_ylabel('PC2')
+    axes[1].set_title('After PCA')
+    plt.show()
+    
+    print(pca.explained_variance_ratio_)
+    
+  
+    #number of components
+    n_pcs = pca.components_.shape[0]
+    print(n_pcs)
+    
+    #get index of most important feature on Each component
+    most_important = [np.abs(pca.components_[i]).argmax() for i in range(n_pcs)]
+    
+    initial_features = []
+    for i in range(0,72):
+        initial_features.append(i)
+    
+    most_important_names = [initial_features[most_important[i]] for i in range(n_pcs)]
+    
+    dic = {'PC{}'.format(i): most_important_names[i] for i in range(n_pcs)}
+
+    # build the dataframe
+    df = pd.DataFrame(dic.items())
+    print("most important features")
+    print(df)
+    
+    
+#important_features(features)
+
+
+#PCA_analysis(features,labels)
 
 
--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -11,7 +11,7 @@ from matplotlib.colors import ListedColormap
 features, labels = data.read_labelled_data("./data/small_data.txt", "./data/small_labels.txt")
 
 #feature selection 
-#removing features with low variance : remove all features that are either 1/0 in more than 80% of samples
+
 
 def univariate_feature_selection():
     selector = SelectKBest(f_classif, k=4) #select the 4 most significant features
@@ -29,107 +29,6 @@ def univariate_feature_selection():
     plt.ylabel(r"Univariate score ($-Log(p_{value})$)")
     plt.show()
 
-#dimension reduction 
-#PCA -> aims to find the directions of maximum variance in high-dimensional data and projects it onto a new subspace with equal or fewer dimensions
+univariate_feature_selection()
 
-def PCA_scratch(features,labels):
-    #standardise the features
-    sc = StandardScaler()
-    X = sc.fit_transform(features)
-    #covariance matrix
-    cov_mat = np.cov(X.T)
-    #perform eigendecomposition which should yield a vector (eigen_vals) consisting of 71 eigenvalues 
-    # and corresponding eigenvectors as columns in 71x71 deimensional matrix (eigen_vecs)
-    eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
-    
-    #calculate cumulative sum of explained variances
-    tot = sum(eigen_vals)
-    var_exp = [(i/tot) for i in sorted(eigen_vals, reverse=True)]
-    cum_var_exp = np.cumsum(var_exp)
-    
-    #plot explained variances
-    plt.bar(range(1,72), var_exp, alpha=0.5,
-        align='center', label='individual explained variance')
-    plt.step(range(1,72), cum_var_exp, where='mid',
-            label='cumulative explained variance')
-    plt.ylabel('Explained variance ratio')
-    plt.xlabel('Principal component index')
-    plt.legend(loc='best')
-    plt.show()
-    
-    #feature transformation
-    
-    #Make a list of (eigenvalue, eigenvector) tuples
-    eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i]) for i in range(len(eigen_vals))]
-    
-    #sort the tuples from high to low
-    eigen_pairs.sort(key = lambda k: k[0], reverse=True)
-    
-    w = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis]))
-    print('Matrix W:\n', w)
-    
-    X[0].dot(w)
-    X_pca = X.dot(w)
-    
-    colors = ['r', 'b', 'g','purple','orange']
-    markers = ['s', 'x', 'o','^','p','*']
-    for l, c, m in zip(np.unique(labels), colors, markers):
-        plt.scatter(X[labels==l, 0], 
-                    X_pca[labels==l, 1], 
-                    c=c, label=l, marker=m) 
-    plt.xlabel('PC 1')
-    plt.ylabel('PC 2')
-    plt.legend(loc='lower left')
-    plt.show()
-    
-    
-PCA_scratch(features,labels)  
-
-#Using the library
-from sklearn.linear_model import LogisticRegression
-
-
-#initialise pca and logistic regression model
-pca = PCA(n_components=2)
-lr = LogisticRegression(multi_class = 'auto', solver ='liblinear')
-    
-#fit and transform data
-X_pca = pca.fit_transform(features)
-lr.fit(X_pca, labels)
-    
-def plot_decision_regions(X, y, classifier, resolution=0.02):
-    # setup marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    # plot class samples
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], 
-                    y=X[y == cl, 1],
-                    alpha=0.6, 
-                    c=[cmap(idx)],
-                    edgecolor='black',
-                    marker=markers[idx], 
-                    label=cl)# plot decision regions for training set
-
-
-plot_decision_regions(X_pca, labels, classifier=lr)
-plt.xlabel('PC 1')
-plt.ylabel('PC 2')
-plt.legend(loc='lower left')
-plt.show()
-
-#clusters -> label is the cluster
     
