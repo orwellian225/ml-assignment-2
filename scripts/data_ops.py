3874a1a23713e36065db62a8d7495776f1406b85
Orwellian <orwellian225@gmail.com>
2023-05-26 14:10:13 +0200

    add: Unlabelled data add: Deserializing network weights

--- scripts/data_ops.py
+++ scripts/data_ops.py
@@ -54,12 +54,41 @@ def read_labelled_data(data_filepath: str, labels_filepath: str, show_log=False)
 
     return np.array(feature_matrix).astype(np.float64), np.array(label_vector).astype(np.uint8)
 
-"""
-Read in labelled data, and then join the feature matrix and label vector into one matrix
+def read_data(data_filepath: str, show_log=False) -> np.array:
+    data_file = open(data_filepath, 'r')
 
-Output data format
-    AUGMENTED DATA MATRIX
-        A joined matrix that has the label for a row as the last column of the row
-"""
-def read_data_augmented(data_filepath: str, labels_filepath: str) -> np.array:
-    raise NotImplementedError
+    feature_matrix = []
+
+    # Count the number of elements in the data file
+    data_count = sum(1 for _ in data_file)
+    data_file.seek(0)
+
+    for i in range(data_count):
+        if show_log:
+            print(f"\rReading data point {i + 1} / {data_count}", end = '')
+
+        data_line = data_file.readline().strip()
+
+        feature_vector = np.array(data_line.split(',')).astype(np.float64)
+
+        feature_matrix.append(feature_vector)
+
+    print(f"\r{data_count} / {data_count} data points read.", " "*10, end='\n')    
+
+    data_file.close()
+    return feature_matrix
+
+def read_network_weights(weights_filepath: str) -> np.array:
+
+    weights_file = open(weights_filepath, 'r')
+    structure = weights_file.readline().strip().split(',')
+    weights = []
+
+    for i in range(len(structure) - 1):
+        wmatrix = []
+        for j in range(int(structure[i + 1])):
+           wmatrix.append(weights_file.readline().strip().split(','))
+
+        weights.append(np.array(wmatrix))
+    
+    return weights 
